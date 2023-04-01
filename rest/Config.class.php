19069d4a00612f1769c6f9532e91a71c3b62e64a
merjemamujic <merjema.mujic@stu.ibu.edu.ba>
2023-04-01 16:45:54 +0200

    mm

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -1,19 +1,24 @@
 <?php
 
-class Config {
-    public static function DB_HOST(){
+class Config
+{
+    public static function DB_HOST()
+    {
         return 'localhost:3306';
     }
 
-    public static function DB_USERNAME(){
+    public static function DB_USERNAME()
+    {
         return 'root';
     }
 
-    public static function DB_PASSWORD(){
+    public static function DB_PASSWORD()
+    {
         return 'obozavamportu';
     }
 
-    public static function DB_SCHEMA(){
+    public static function DB_SCHEMA()
+    {
         return 'skincare';
     }
 }
