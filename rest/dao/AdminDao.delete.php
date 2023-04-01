6f3e105a3b0ea4ffcac4acbbbcf8704e14ff6b11
merjemamujic <merjema.mujic@stu.ibu.edu.ba>
2023-04-01 16:57:46 +0200

    n

--- rest/dao/AdminDao.delete.php
+++ rest/dao/AdminDao.delete.php
@@ -8,20 +8,20 @@ $scheme = "skincare";
 //INSERT INTO DB
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$scheme", $username, $password);
-    
+
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql= "DELETE FROM skincare WHERE id_admin=6";
+    $sql = "DELETE FROM skincare WHERE id_admin=6";
     $conn->exec($sql);
     echo "New record has been deleted";
-    
 
-   // echo "Connected successfully";
 
-    
-    
-}    catch (PDOException $e) {
-    echo $sql ."<br>" .$e->getMessage();
+    // echo "Connected successfully";
+
+
+
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
     //echo "Connection failed:" .$e -> getMessage();
 }
 
